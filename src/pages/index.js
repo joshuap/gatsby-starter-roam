0d841d57bac008c4bf29aaf1f4990f1b9ad9324a
Joshua Wood <josh@joshuawood.net>
2020-05-07 22:41:43 -0700

    List pages for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default function IndexPage({ data }) {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>
+          All Pages
+        </h1>
+        <h4>{data.allRoamJson.totalCount} Pages</h4>
+        {data.allRoamJson.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}>
+              <h3>
+                {node.title}{" "}
+              </h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allRoamJson {
+      totalCount
+      edges {
+        node {
+          id
+          title
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
